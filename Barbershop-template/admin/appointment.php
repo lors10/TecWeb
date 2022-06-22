070a8f583c1f652a0ef6b395c170bafd979aba77
Lorenzo Salvi <lorenzosalvi@MacBook-Air-di-Lorenzo.local>
2022-06-22 17:49:49 +0200

    implementazione preliminare logica pagina appuntamento lato admin

--- Barbershop-template/admin/appointment.php
+++ Barbershop-template/admin/appointment.php
@@ -6,6 +6,68 @@
 
     $main = new Template("design/index.html");
     $appointment = new Template("design/appointments.html");
+    $next_appointment_table = new Template("design/appointments-next-table.html");
+
+    $stmt = $connection->query("SELECT appuntamento.idAppuntamento, appuntamento.inizioAppuntamento, appuntamento.idUtente,
+                                                 prenotazione.idAppuntamento, prenotazione.idAttivita,
+                                                 attivita.idAttivita, attivita.nomeAttivita
+                                                 FROM appuntamento
+                                                 INNER JOIN prenotazione
+                                                 ON appuntamento.idAppuntamento = prenotazione.idAppuntamento
+                                                 INNER JOIN attivita
+                                                 ON prenotazione.idAttivita = attivita.idAttivita
+                                                 WHERE appuntamento.inizioAppuntamento >= current_date");
+
+    if (!$stmt) {
+
+        //error
+    }
+
+    do {
+
+        $data = $stmt->fetch_assoc();
+        if ($data){
+            foreach ($data as $key => $value) {
+                $next_appointment_table->setContent($key, $value);
+            }
+        }
+    } while ($data);
+
+    $all_appointment_table = new Template("design/appointments-all-table.html");
+
+    // query per estrapolare dati di:
+    // - appuntamento
+    // - utente
+    // - servizio (attivita)
+
+    $stmt = $connection->query("SELECT appuntamento.idAppuntamento, appuntamento.inizioAppuntamento, appuntamento.idUtente,
+                                             prenotazione.idAppuntamento, prenotazione.idAttivita,
+                                             attivita.idAttivita, attivita.nomeAttivita
+                                             FROM appuntamento
+                                             INNER JOIN prenotazione
+                                             ON appuntamento.idAppuntamento = prenotazione.idAppuntamento
+                                             INNER JOIN attivita
+                                             ON prenotazione.idAttivita = attivita.idAttivita
+                                             /*WHERE appuntamento.inizioAppuntamento >= current_date*/");
+
+    if (!$stmt) {
+
+        //error
+    }
+
+    do {
+
+        $data = $stmt->fetch_assoc();
+        if ($data){
+            foreach ($data as $key => $value) {
+                $all_appointment_table->setContent($key, $value);
+            }
+        }
+    } while ($data);
+
+
+
+    $cancelled_appointment_table = new Template("design/appointments-cancelled-table.html");
 
     $stmt = $connection->query("SELECT * FROM attivita");
 
@@ -42,6 +104,9 @@
     $main->setContent("clientsCount", $clientsCount);
 
 
+    $appointment->setContent("appointmentNexttable", $next_appointment_table->get());
+    $appointment->setContent("appointmentAlltable", $all_appointment_table->get());
+    $appointment->setContent("appointmentCancelledtable", $cancelled_appointment_table->get());
     $main->setContent("appointment", $appointment->get());
     $main->setContent("loggedUser", $_SESSION['name']);
     $main->close();
