ec0310a1f6d189116710fc4e78d5b5435cad53de
Lorenzo Salvi <lorenzosalvi@MacBook-Air-di-Lorenzo.local>
2022-06-29 17:34:36 +0200

    implementazione operazione di update

--- Barbershop-template/admin/service_category_edit.php
+++ Barbershop-template/admin/service_category_edit.php
@@ -4,6 +4,9 @@
     require ("../include/template2.inc.php");
     require ("../include/session-start.php");
 
+    $main = new Template("design/index.html");
+    $edit_category = new Template("design/service-category-edit.html");
+
 
     if (!isset($_REQUEST['state'])) {
         $_REQUEST['state'] = 0;
@@ -13,7 +16,18 @@
 
         case 0:
 
-            //
+            // estraggo i dati della tabella servizi
+            $stmt = $connection->query("SELECT idCategoria, nomeCategoria, descrizioneCategoria 
+                                                FROM categoriaAttivita
+                                                WHERE idCategoria = {$_REQUEST['edit']}");
+
+            while ($data = $stmt->fetch_assoc()) {
+
+                foreach ($data as $key => $value) {
+
+                    $edit_category->setContent($key,$value);
+                }
+            }
 
             break;
 
@@ -24,21 +38,60 @@
             // tornare alla home
 
             $stmt = $connection->query("UPDATE categoriaAttivita SET 
-                                                    nomeCategoria = \"{$_REQUEST['category_name']}\",
-                                                    descrizioneCategoria = \"{$_REQUEST['category_description']}\"
-                                                    WHERE idCategoria = {$_REQUEST['edit']}");
+                                                    nomeCategoria = \"{$_REQUEST['category_name_update']}\",
+                                                    descrizioneCategoria = \"{$_REQUEST['category_description_update']}\"
+                                                    WHERE idCategoria = {$_REQUEST['category_id']}");
 
             if ($stmt == 1) {
 
                 echo "Categoria aggiunta con successo!";
 
-                header("Location: index.php");
+                header("Location: service_category.php");
             } else {
 
                 // check errore
                 echo "Errore: " . $stmt . '<br />' . $connection->connect_error;
             }
+
+            break;
     }
+
+    $stmt = $connection->query("SELECT * FROM attivita");
+
+    $serviceCount = $stmt->num_rows;
+
+    $main->setContent("serviceCount", $serviceCount);
+
+
+    $stmt = $connection->query("SELECT * FROM dipendenti");
+
+    $employeesCount = $stmt->num_rows;
+
+    $main->setContent("employeesCount", $employeesCount);
+
+
+    $stmt = $connection->query("SELECT * FROM appuntamento");
+
+    $data = $stmt->num_rows;
+
+    $main->setContent("appointmentCount", $data);
+
+
+    $stmt = $connection->query("SELECT utenti.idUtente, utenti.nomeUtente, utenti.cognomeUtente, utenti.cellulareUtente, utenti.emailUtente,
+                                                utentiGruppi.idUtente, utentiGruppi.idGruppo
+                                                FROM utenti
+                                                LEFT JOIN utentiGruppi
+                                                ON utenti.idUtente = utentiGruppi.idUtente
+                                                /*WHERE utentiGruppi.idGruppo = 2*/");
+
+    $clientsCount = $stmt->num_rows;
+
+    $main->setContent("clientsCount", $clientsCount);
+
+
+    $main->setContent("edit_category", $edit_category->get());
+    $main->setContent("loggedUser", $_SESSION['name']);
+    $main->close();
 
 
 ?>
